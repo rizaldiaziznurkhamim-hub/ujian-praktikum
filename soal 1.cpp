e783f52b41f9390caaf4b70e19e12c1187806bda
rizaldiaziznurkhamim-hub <rizaldiaziznurkhamim@gmail.com>
2025-12-18 08:28:06 +0700

    Create soal 1.cpp

---
+++ soal 1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int matrik[4][4] = {
+		{1, 2, 3, 4},
+		{5, 6, 7, 8},
+		{9, 10, 11, 12},
+		{13, 14, 15, 16}
+	};
+	cout << "Matrik 4x4 :" << endl;
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			cout << matrik[i][j] << "\t";
+		}
+		cout << endl;
+	}
+	return 0;
+}
